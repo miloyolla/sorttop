b99663e3bf573be83be038914e47bc7405615f21
vitor pavani <vitorpavani@poli.ufrj.br>
2019-08-28 16:35:38 -0300

    adiciona as estruturas de dados

---
+++ data.h
@@ -0,0 +1,18 @@
+#ifndef DATA_STRUCT
+#define DATA_STRUCT
+
+struct data{
+    char[4] ano;
+    char[64] statusProc;
+    char[32] superintend;
+    char[32] numeroProc; 
+    char[32] autoInfra;
+    char[32] cpfCnpj;
+    char[128] razSocial;
+}
+struct index{
+    campo[128];
+    int index;
+}
+
+#endif

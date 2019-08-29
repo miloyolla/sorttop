bbea1915be6f9cc054b2636532dd7450b5e7ee71
vitor pavani <vitorpavani@poli.ufrj.br>
2019-08-29 12:58:01 -0300

    adiciona os ultimos campos que faltavam na struct

--- data.h
+++ data.h
@@ -1,18 +1,22 @@
 #ifndef DATA_STRUCT
 #define DATA_STRUCT
 
-struct data{
+struct data
+{
     char ano[4];
     char statusProc[64];
     char superintend[32];
-    char numeroProc[32]; 
+    char numeroProc[32];
     char autoInfra[32];
     char cpfCnpj[32];
     char razSocial[128];
-}
-struct index{
-    campo[128];
+    char vencimento[16];
+    char valorMultaApl[32];
+};
+struct index
+{
+    char campo[128];
     int index;
-}
+};
 
 #endif

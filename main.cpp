499bf25fe7c43285c514a05f9c430b44be45126a
vitor pavani <vitorpavani@poli.ufrj.br>
2019-08-31 17:11:05 -0300

    adiciona o esqueleto de um menu REPL

--- main.cpp
+++ main.cpp
@@ -14,12 +14,54 @@ int main(int argc, char *argv[])
 {
     std::vector<csvio::data> dataVec;
     std::vector<csvio::index> index;
+
+    char option;
+    bool reploop = true;
+
+    if (argc > 2)
+    {
+        csvio::ReadCsv(argv[1], dataVec);
+    }
+    // basic REPL menu
+    do
+    {
+
+        std::cout << "------------------------------------------------"
+                  << std::endl
+                  << "a - ler um arquivo."
+                  << std::endl
+                  << "c - escolher campo de ordenação. "
+                  << std::endl
+                  << "s - sair."
+                  << std::endl
+                  << "------------------------------------------------"
+                  << std::endl
+                  << "Entre com sua opção:()"
+                  << std::endl;
+        std::cin >> option;
+        switch (option)
+        {
+        case 'a':
+            break;
+
+        case 's':
+            reploop = false;
+            break;
+
+        default:
+            std::cout << "opção inválida" << std::endl;
+            break;
+        }
+        std::cout << " " << std::endl;
+    } while (reploop);
+    /* 
     
-    csvio::ReadCsv(argv[1], dataVec);
-    
+
     std::cout << "linhas lidas: " << dataVec.size() << std::endl;
-    for (auto& data : dataVec)
+    for (auto &data : dataVec)
         std::cout << data.razSocial << " " << data.cpfCnpj << std::endl;
+
+    */
 
     return 0;
 }

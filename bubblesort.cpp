1f055f3d83af0e6c16eb6f18148a241e8ffe399a
Vitor Pavani <vitorpavani@poli.ufrj.br>
2019-09-06 23:39:25 -0300

    modifica o bubble sort para mover o elemento inteiro do vector

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -18,16 +18,16 @@ void bubble(std::vector<csvio::index> &index)
     while (troca == true)
     {
         troca = false;
-        std::string aux;
+        csvio::index aux;
 
         for (int i = 0; i <= index.size(); i++)
         {
             if (index[i].campo > index[i + 1].campo)
             {
                 troca = true;
-                aux = index[i].campo;
-                index[i].campo = index[i + 1].campo;
-                index[i + 1].campo = aux;
+                aux = index[i];
+                index[i] = index[i + 1];
+                index[i + 1] = aux;
             }
         }
     }
